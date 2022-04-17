c099a8acc6d5f251aa9ad766cdc34e5267836288
Monir Hossain Abde Rabby <96588176+monirhabderabby@users.noreply.github.com>
2022-04-17 08:15:18 +0600

    Banner and header sturcture completed

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Banner.css";
+import gallary from "../Assets/gallary.jpg";
+import { Link } from "react-router-dom";
+
+const Banner = () => {
+  return (
+    <div className="container">
+      <div className="row banner">
+        <div className="col-lg-6">
+          <div className="content">
+            <h1>Every moment of your life beautiful</h1>
+            <p>
+              Your each movement, your each emotion is nothing but a beautiful
+              picture and I just on to hold on those moments with our camera and
+              make it a piece of art with my adroit editing
+            </p>
+            <Link to={'/gallary'} className="view-gallary-btn">View Gallary</Link>
+          </div>
+        </div>
+        <div className="col-lg-6">
+          <div className="gallary">
+            <img className="img-fluid" src={gallary} alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
 import Navbar from '../Navbar/Navbar';
 import './Header.css'
 
 const Header = () => {
     return (
-        <div>
+        <div className='header'>
             <Navbar></Navbar>
+            <Banner></Banner>
         </div>
     );
 };
