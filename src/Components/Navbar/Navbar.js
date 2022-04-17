72fff293bce7788a8c4b07b2521c25f5a4584f69
Monir Hossain Abde Rabby <96588176+monirhabderabby@users.noreply.github.com>
2022-04-17 07:19:57 +0600

    Navbar structure ready

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,11 +1,23 @@
 import React from "react";
-import { Container, Nav, NavDropdown } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <div>
-      
+      <nav class="container">
+            <div class="navbar">
+                <div class="logo">
+                    <h2>NIX - Photography</h2>
+                </div>
+                <div class="menu">
+                  <Link to={'/'} className="menubar">Home</Link>
+                  <Link to={'/services'} className="menubar">Services</Link>
+                  <Link to={'/about me'} className="menubar">About Me</Link>
+                  <Link to={'/login'} className="menubar">Login</Link>
+                </div>
+            </div>
+        </nav>
     </div>
   );
 };
