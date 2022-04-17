717968b5af49bd47e09b8069b83bd70ae3ad1e68
Monir Hossain Abde Rabby <96588176+monirhabderabby@users.noreply.github.com>
2022-04-17 21:19:57 +0600

    login and signup structure ready

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import { Route, Routes } from 'react-router-dom';
 import Packages from './Components/Packages/Packages';
 import Header from './Components/Header/Header';
 import Checkout from './Components/Checkout/Checkout';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import Login from './Components/Login/Login';
+import SignUp from './Components/SignUp/SignUp';
 
 function App() {
   const [buyPackage, setPackage] = useState({})
@@ -18,6 +20,8 @@ function App() {
         <Route path='/' element={<Header></Header>}></Route>
         <Route path='/packages' element={<Packages checkOut={checkOut}></Packages>}></Route>
         <Route path='/checkout' element={<Checkout buyPackage={buyPackage}></Checkout>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='signup' element={<SignUp></SignUp>}></Route>
       </Routes>
     </div>
   );
--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -1,26 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Navbar from '../Navbar/Navbar';
 import google from '../Assets/google.svg'
-import './Login.css'
-import { Link } from 'react-router-dom';
 
-const Login = () => {
+const SignUp = () => {
     return (
         <div className='login-container'>
             <Navbar></Navbar>
             <div className="login-form">
                 <form className='form'>
-                    <h1>Login</h1>
+                    <h1>SignUp</h1>
                     <div className='input-field'>
                         <input type="email" name="email" placeholder='Email' required/>
                     </div>
                     <div className='input-field'>
                         <input type="password" name="password" placeholder='Password' required/>
                     </div>
-                    <small className='reset-btn'>Reset Password</small> <br />
                     <input type="submit" value="LOGIN" className='login-btn'/>
-                    <p>I am new? <Link to="/signup" className='signup-btn'>Create an Account</Link></p>
-
+                    <p>Already have an account? <Link to="/login" className='signup-btn'>Login</Link></p>
                     <p className='or'>------ or ------</p>
                     
                     <div className='input-field'>
@@ -35,4 +32,4 @@ const Login = () => {
     );
 };
 
-export default Login;
+export default SignUp;
